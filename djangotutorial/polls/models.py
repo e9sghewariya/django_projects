7e7b3890251277d373470605d773b5d2f5e52269
Shreyansh Ghewariya <sghewariya@enine.dev>
2025-08-11 13:00:48 +0530

    feat(django): Demo django project --Based on django documentation --Still work in progress

---
+++ djangotutorial/polls/models.py
@@ -0,0 +1,29 @@
+import datetime
+
+from django.db import models
+from django.utils import timezone
+
+
+# Create your models here.
+
+class Question(models.Model):
+    
+    #to string method
+    def __str__(self):
+        return self.question_text
+    question_text = models.CharField(max_length=200)
+    published_date = models.DateTimeField("Date Published")
+    
+    
+    def was_published_recently(self):
+        return self.published_date >= timezone.now() - datetime.timedelta(days=1)
+    
+class Choice(models.Model):
+    
+    def __str__(self):
+        return self.choice_text
+    
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
