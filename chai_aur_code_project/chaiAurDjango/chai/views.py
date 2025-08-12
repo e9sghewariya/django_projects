4b4f3fdadc3b666e87a0c6a60a5d0d1033929507
Shreyansh Ghewariya <sghewariya@enine.dev>
2025-08-12 09:44:54 +0530

    feat(chai_aur_django): Project based on learnings on video -- admin.py: To get models registered in admin panel -- models.py: Contains models for chai app: 		ChaiVarity: Model for storing different type of Chai 		ChaiReview: Model for storing detail about every chai, 			    used this model to understand One to Many 			    relationship 		Store:	    Model for a Chai store, used this model to 			    understand Many to Many relationship 		ChaiCertificate: Model to store when chai was made. 				 used this model to understand One to 				 One relationship -- views.py : Contains api call to accept request and send response 	      API calls for: 		-- to get all Chai 		-- to get detali about every chai 		-- to get at which store what chai is available [used 		   form to understand their working]

---
+++ chai_aur_code_project/chaiAurDjango/chai/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, get_object_or_404
+from .models import ChaiVarity, Store
+from .forms import ChaiVarityForm
+
+# Create your views here.
+
+def all_chai(request):
+    chais = ChaiVarity.objects.all()
+    return render(request, 'chai/all_chai.html', { 'chais': chais})
+
+def chai_detail(request, chai_id):
+    chai = get_object_or_404(ChaiVarity, pk=chai_id)
+    return render(request,'chai/chai_detail.html', {'chai': chai})
+
+def chai_store_view(request):
+    stores = None
+    
+    if request.method == 'POST':
+        form = ChaiVarityForm(request.POST)
+        
+        if form.is_valid():
+           chai_variety = form.cleaned_data['chai_variety']
+           stores = Store.objects.filter(chai_varieties=chai_variety)
+           
+    else:
+        form = ChaiVarityForm()
+    return render(request, 'chai/chai_stores.html', {'stores': stores, 'form':form})
+
